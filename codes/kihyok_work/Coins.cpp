68a97222ab0775d6123c65118e15f44e38a5e443
hudson-k-i <kihyok.im.1996@gmail.com>
2022-11-26 09:42:30 +0900

    commit します

---
+++ codes/kihyok_work/Coins.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+  int A,B,C,X;
+
+  cin >> A >> B >> C >> X ;
+
+  int count=0;
+  for(int i=0;i<A;i++){
+    for(int j=0;j<B;j++){
+        for(int k=0;k<C;k++){
+            if(500 * i + 100 * B + 50 * C == X){
+                count++;
+            }
+        }
+    }  
+  }
+  cout << count;
+
+}
+
+
